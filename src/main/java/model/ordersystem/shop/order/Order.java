0ba3a64746f16c9847fe52d3e5ba42c73743dbdc
Alan <baranalaan@gmail.com>
2022-07-11 12:51:39 +0200

    add assertj to dependency, add Order and OrderRepo

---
+++ src/main/java/model/ordersystem/shop/order/Order.java
@@ -0,0 +1,11 @@
+package model.ordersystem.shop.order;
+
+import model.ordersystem.shop.product.Product;
+
+import java.util.List;
+
+public record Order(
+        int id;
+        List<Product> products
+) {
+}
---
+++ src/main/java/model/ordersystem/shop/order/OrderRepo.java
@@ -0,0 +1,4 @@
+package model.ordersystem.shop.order;
+
+public class OrderRepo {
+}
