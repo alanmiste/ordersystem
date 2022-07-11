c90b15221daa4e7f88d8eea9b5b499522cb4bef1
Alan <baranalaan@gmail.com>
2022-07-11 13:05:07 +0200

    fix ShopService

--- src/main/java/model/ordersystem/ShopService.java
+++ src/main/java/model/ordersystem/ShopService.java
@@ -1,17 +1,48 @@
 package model.ordersystem;
 
+import model.ordersystem.shop.order.Order;
+import model.ordersystem.shop.order.OrderRepo;
 import model.ordersystem.shop.product.Product;
 import model.ordersystem.shop.product.ProductRepo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShopService {
-    private  ProductRepo productRepo;
+    private final ProductRepo productRepo;
+    private final OrderRepo orderRepo;
 
-
-    public ShopService(ProductRepo productRepo){
+    public ShopService(
+            ProductRepo productRepo,
+            OrderRepo orderRepo) {
         this.productRepo = productRepo;
+        this.orderRepo = orderRepo;
     }
 
     public Product getProduct(int id) {
         return productRepo.getProduct(id);
+    }
+
+    public List<Product> listProducts() {
+        return productRepo.listProducts();
+    }
+
+    public void addOrder(int orderId, List<Integer> productIds) {
+        List<Product> products = new ArrayList<>();
+        for (int productId : productIds) {
+            Product product = productRepo.getProduct(productId);
+            products.add(product);
+        }
+
+        Order order = new Order(orderId, products);
+        orderRepo.addOrder(order);
+    }
+
+    public Order getOrder(int orderId) {
+        return orderRepo.getOrder(orderId);
+    }
+
+    public List<Order> listOrders() {
+        return orderRepo.listOrders();
     }
 }
